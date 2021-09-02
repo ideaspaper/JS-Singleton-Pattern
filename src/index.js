65839c1f437e67ad910452754ccebfd2400c0b1d
ideaspaper <agni.infinity@gmail.com>
2021-09-02 10:42:29 +0700

    Remove index.js from source

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const Singleton = require('./singleton');
-
-const instance1 = Singleton.getInstance();
-const instance2 = Singleton.getInstance();
-
-console.log(instance1);
-console.log(instance2);
-console.log(instance1 === instance2);
-
